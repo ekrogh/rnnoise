d15d761d08a944bc1dc047f8399c762c542ad45f
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2025-09-18 23:39:24 +0200

    Removed LFS'

--- src/rnnoise_data.c
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:27ca7a5e7cbfa7583f30416baea83282bebecb39645e7e0c083dd6fec75ddf3f
-size 36411766
--- src/rnnoise_data_little.c
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cf38e20a2c78e3e6080f848f3c045ea2c646abdea985e2097228e8ec3c803bef
-size 14928260
